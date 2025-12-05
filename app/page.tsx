40da979a7370f5d978e33169a814c9cdd00826fc
mung96 <mung5079@gmail.com>
2025-12-05 12:50:48 +0900

    feat: Button 컴포넌트 구현 (primary만)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@shared/ui";
+
 export default function Home() {
   return (
     <div>
       <p className="text-title font-bold">프리텐다드X</p>
+      <Button priority="primary">Primary</Button>
+      <Button priority="secondary">Secondary</Button>
+      <Button priority="tertiary">Tertiary</Button>
+
       <p className="text-title font-medium font-pretendard">프리텐다드O</p>
     </div>
   );
