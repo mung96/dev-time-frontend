3d6aba586ae1eb25beecf0b2d8fd2c2fa11e178e
mung96 <mung5079@gmail.com>
2025-12-16 09:18:39 +0900

    feat: 할일 편집 기능 구현

--- src/pages/timer/ui/create-todo-page.tsx
+++ src/pages/timer/ui/create-todo-page.tsx
@@ -100,22 +100,56 @@ export const TodoItem = ({
   onEdit: (newTask: Task) => void;
   onDelete: () => void;
 }) => {
-  const [isEditMode, setIsEditMode] = useState(false);
+  const [content, setContent] = useState(task.content);
+  const [mode, setMode] = useState<"read" | "edit">("read");
   return (
     <div className="w-[568px] h-18 bg-primary flex items-center rounded-md px-6 py-[26px] justify-between">
-      <p className="text-white text-body">{task.content}</p>
-
-      <div className="flex gap-4">
-        <Image width={24} height={24} src="/icons/edit.svg" alt="편집" />
-        <Image
-          width={24}
-          height={24}
-          onClick={onDelete}
-          src="/icons/trash.svg"
-          alt="삭제"
-        />
+      {/* 콘텐츠 부분 */}
+      <div>
+        {mode === "read" && (
+          <p className="text-white text-body">{task.content}</p>
+        )}
+        {mode === "edit" && (
+          <input value={content} onChange={(e) => setContent(e.target.value)} />
+        )}
       </div>
-      {/* <Image width={24} height={24} src="/icons/check.svg" alt="끝내기" /> */}
+
+      {/* 우측 */}
+      <div>
+        {mode === "read" && (
+          <div className="flex gap-4">
+            <Image
+              width={24}
+              height={24}
+              src="/icons/edit.svg"
+              alt="편집"
+              onClick={() => setMode("edit")}
+            />
+            <Image
+              width={24}
+              height={24}
+              onClick={onDelete}
+              src="/icons/trash.svg"
+              alt="삭제"
+            />
+          </div>
+        )}
+
+        {mode === "edit" && (
+          <Image
+            width={24}
+            height={24}
+            src="/icons/check.svg"
+            alt="끝내기"
+            onClick={() =>
+              onEdit({
+                content: content,
+                isCompleted: task.isCompleted,
+              })
+            }
+          />
+        )}
+      </div>
     </div>
   );
 };
