49835de37558096995cf1e4f28f9825141b81a58
mung96 <mung5079@gmail.com>
2025-12-22 09:39:16 +0900

    refactor: splitTime 생성 함수 리팩토링

--- src/pages/timer/lib/index.ts
+++ src/pages/timer/lib/index.ts
@@ -20,53 +20,44 @@ export const getSplitTime = (
   timeSpent: number;
 }[] => {
   const ONE_DAY_MS = 24 * 60 * 60 * 1000;
-  const nowDateTime = new Date();
+  const now = new Date();
 
   // 날짜만 비교 (연, 월, 일)
-  const startDate = getDateOnly(startDateTime);
-  const todayStart = getDateOnly(nowDateTime);
-  const isDateChange = startDate.getTime() !== todayStart.getTime();
+  const startOfFirstDay = getStartOfDay(startDateTime);
+  const startOfToday = getStartOfDay(now);
 
-  if (!isDateChange) {
+  if (startOfFirstDay.getTime() === startOfToday.getTime()) {
     return [
       {
-        date: startDate.toISOString(),
-        timeSpent: getTimeSpent(startDateTime, nowDateTime),
+        date: startOfFirstDay.toISOString(),
+        timeSpent: getTimeSpent(startDateTime, now),
       },
     ];
   }
 
   const splitTimes = [];
-  const nextDayStart = new Date(
-    startDateTime.getFullYear(),
-    startDateTime.getMonth(),
-    startDateTime.getDate() + 1,
-    0,
-    0,
-    0,
-    0
-  );
+  const startOfNextDay = addDays(startOfFirstDay, 1);
 
   //첫날
   splitTimes.push({
-    date: startDate.toISOString(),
-    timeSpent: getTimeSpent(startDateTime, nextDayStart),
+    date: startOfFirstDay.toISOString(),
+    timeSpent: getTimeSpent(startDateTime, startOfNextDay),
   });
 
   //중간날
-  let currentDate = new Date(nextDayStart);
-  while (currentDate.getTime() < todayStart.getTime()) {
+  let currentDay = new Date(startOfNextDay);
+  while (currentDay.getTime() < startOfToday.getTime()) {
     splitTimes.push({
-      date: currentDate.toISOString(),
+      date: currentDay.toISOString(),
       timeSpent: ONE_DAY_MS,
     });
-    currentDate = new Date(currentDate.getTime() + ONE_DAY_MS);
+    currentDay = addDays(currentDay, 1);
   }
 
   //마지막날
   splitTimes.push({
-    date: todayStart.toISOString(),
-    timeSpent: getTimeSpent(todayStart, nowDateTime),
+    date: startOfToday.toISOString(),
+    timeSpent: getTimeSpent(startOfToday, now),
   });
   return splitTimes;
 };
@@ -74,6 +65,12 @@ export const getSplitTime = (
 const getTimeSpent = (startDateTime: Date, endDateTime: Date) => {
   return endDateTime.getTime() - startDateTime.getTime();
 };
-const getDateOnly = (date: Date): Date => {
+const getStartOfDay = (date: Date): Date => {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+};
+
+const addDays = (date: Date, days: number) => {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days);
+  return newDate;
 };
