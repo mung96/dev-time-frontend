81f5577445ab5515ddc5563a8bc8b8417e4015b6
mung96 <mung5079@gmail.com>
2025-12-27 15:57:29 +0900

    refactor: splitTimes를 getState로 가져오도록 변경

--- src/app/providers/timer-provider.tsx
+++ src/app/providers/timer-provider.tsx
@@ -4,17 +4,13 @@ import { useTimerStore } from "@pages/timer/model/use-timer-store";
 import { useUpdateTimer } from "@pages/timer/api/use-update-timer";
 import { useEffect, useRef } from "react";
 
-// const POLLING_INTERVAL_MS = 10 * 60 * 1000; // 10분
-const POLLING_INTERVAL_MS = 5 * 1000; // TODO: 테스트용 5초
+const POLLING_INTERVAL_MS = 10 * 60 * 1000; // 10분
+// const POLLING_INTERVAL_MS = 5 * 1000; // TODO: 테스트용 5초
 
 export function TimerProvider({ children }: { children: React.ReactNode }) {
-  const { isRunning, timerId, splitTimes } = useTimerStore();
+  const { isRunning, timerId } = useTimerStore();
   const { mutate: updateTimer } = useUpdateTimer();
   const schedulerRef = useRef<ReturnType<typeof setInterval> | null>(null);
-
-  useEffect(() => {
-    console.log("isRunning", isRunning);
-  }, [isRunning]);
 
   useEffect(() => {
     if (!timerId) return;
@@ -28,9 +24,9 @@ export function TimerProvider({ children }: { children: React.ReactNode }) {
       return;
     }
 
-    //실행중이면 스케줄러 시작 (10분마다 업데이트)
-    console.log("timer 스케줄러 실행");
+    //실행중이면 스케줄러 시작
     schedulerRef.current = setInterval(() => {
+      const { splitTimes } = useTimerStore.getState();
       updateTimer({
         timerId,
         payload: {
@@ -45,7 +41,7 @@ export function TimerProvider({ children }: { children: React.ReactNode }) {
         schedulerRef.current = null;
       }
     };
-  }, [isRunning, timerId]);
+  }, [isRunning, timerId, updateTimer]);
 
   return <>{children}</>;
 }
