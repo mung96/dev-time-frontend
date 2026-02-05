f6dea9fdab285965add004f278e55de4971d0acc
mung96 <mung5079@gmail.com>
2026-02-05 09:39:09 +0900

    refactor: button style cva적용

--- frontend/src/shared/ui/button/index.tsx
+++ frontend/src/shared/ui/button/index.tsx
@@ -1,37 +1,44 @@
 "use client";
 
 import { cn } from "@shared/lib/tailwind";
+import { cva, type VariantProps } from "class-variance-authority";
 
-interface ButtonProps extends React.ComponentProps<"button"> {
-  priority: "primary" | "secondary" | "tertiary";
-}
+type ButtonProps = React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants>;
 
 export const Button = ({ priority, className, ...rest }: ButtonProps) => {
   return (
-    <button
-      className={cn(
-        [
-          "w-full h-12 px-4 py-3 rounded-sm text-subtitle font-semibold relative", //기본 CSS
-          "before:content-[''] before:absolute before:inset-0 before:bg-black/10 before:opacity-0 before:rounded-sm before:transition-opacity", //hover, active시 사용
-          "enabled:hover:before:opacity-100", // hover
-          "enabled:active:before:opacity-100", // active
-          "focus:ring-1 focus:ring-fuchsia", // focus
-        ],
-        priority === "primary" && [
-          "bg-primary text-white ", // 기본
-          "disabled:bg-gray-400 disabled:text-gray-300", // disabled
-        ],
-        priority === "secondary" && [
-          "bg-primary/10 text-primary ",
-          "disabled:bg-gray-200 disabled:text-gray-400",
-        ],
-        priority === "tertiary" && [
-          "bg-gray-50 text-primary ",
-          "disabled:bg-gray-200 disabled:text-gray-400",
-        ],
-        className
-      )}
-      {...rest}
-    />
+    <button className={cn(buttonVariants({ priority }), className)} {...rest} />
   );
 };
+
+const buttonVariants = cva(
+  [
+    "w-full h-12 px-4 py-3 rounded-sm text-subtitle font-semibold relative", //기본 CSS
+    "before:content-[''] before:absolute before:inset-0 before:bg-black/10 before:opacity-0 before:rounded-sm before:transition-opacity", //hover, active시 사용
+    "enabled:hover:before:opacity-100", // hover
+    "enabled:active:before:opacity-100", // active
+    "focus:ring-1 focus:ring-fuchsia", // focus
+  ],
+  {
+    variants: {
+      priority: {
+        primary: [
+          "bg-primary text-white", // 기본
+          "disabled:bg-gray-400 disabled:text-gray-300", // disabled
+        ],
+        secondary: [
+          "bg-primary/10 text-primary ", // 기본
+          "disabled:bg-gray-200 disabled:text-gray-400", // disabled
+        ],
+        tertiary: [
+          "bg-gray-50 text-primary ", // 기본
+          "disabled:bg-gray-200 disabled:text-gray-400", // disabled
+        ],
+      },
+    },
+    defaultVariants: {
+      priority: "primary",
+    },
+  }
+);
