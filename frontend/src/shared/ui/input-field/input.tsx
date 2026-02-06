996042fffee564e699ed9b177a10043e1922540a
mung96 <mung5079@gmail.com>
2026-02-06 09:54:33 +0900

    refactor: input css를 cva, cn으로 교체

---
+++ frontend/src/shared/ui/input-field/input.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@shared/lib/tailwind";
+import { cva, VariantProps } from "class-variance-authority";
+import { ComponentProps } from "react";
+
+type InputProps = VariantProps<typeof InputVariants> & ComponentProps<"input">;
+
+const InputVariants = cva(
+  [
+    "w-full h-11 flex-1 min-w-30 bg-gray-50 px-4 py-3 text-gray-600 text-body font-medium rounded-sm",
+    "placeholder:text-gray-300 placeholder:text-body placeholder:font-medium", //placeholder
+    "focus:text-gray-800", //focus
+  ],
+  {
+    variants: {
+      isError: {
+        true: ["border border-negative"],
+      },
+    },
+  }
+);
+
+export const Input = ({ isError, className, ...props }: InputProps) => {
+  return (
+    <input className={cn(InputVariants({ isError }), className)} {...props} />
+  );
+};
