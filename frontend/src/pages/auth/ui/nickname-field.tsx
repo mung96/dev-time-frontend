938ed94dc480aee38ee33963a4e67a38c1652d0e
mung96 <mung5079@gmail.com>
2026-02-06 21:37:17 +0900

    refactor: nickname field text-field로 변경

--- frontend/src/pages/auth/ui/nickname-field.tsx
+++ frontend/src/pages/auth/ui/nickname-field.tsx
@@ -1,9 +1,7 @@
-import { TextFieldButton } from "@shared/ui/input-field/text-field-button";
 import { checkNickname } from "@pages/auth/api/check-nickname";
 import { ApiError, HttpStatus } from "@shared/api";
-import { TextField as InputField } from "@shared/ui/input-field";
 import { useFormContext, useFormState } from "react-hook-form";
-import { HelperText } from "@shared/ui/text-field/helper-text";
+import { TextField } from "@shared/ui/text-field";
 
 type NicknameFieldProps = {
   isValidDuplicateNickname: boolean;
@@ -21,67 +19,60 @@ export const NicknameField = ({
     control,
   });
   return (
-    <InputField
-      label={"닉네임"}
-      placeholder="닉네임을 입력해 주세요."
-      {...register("nickname", {
-        onChange: () => setIsValidDuplicateNickname(false),
-      })}
-      button={
-        <TextFieldButton
-          type="button"
-          disabled={
-            !touchedFields.nickname ||
-            errors.nickname?.type === "invalid_format"
-          }
-          onClick={() => {
-            const nickname = getValues("nickname");
-            checkNickname(nickname)
-              .then((response) => {
-                if (response.available) {
-                  setIsValidDuplicateNickname(true);
-                } else {
-                  setError("nickname", {
-                    type: "duplicate",
-                    message: response.message,
+    <>
+      <TextField
+        id={"nickname"}
+        error={!!touchedFields.nickname ? errors.nickname?.message : undefined}
+        success={
+          isValidDuplicateNickname ? "사용 가능한 닉네임입니다." : undefined
+        }
+      >
+        <div className="flex flex-col gap-2">
+          <TextField.Label label="닉네임" />
+          <div className="flex gap-3">
+            <TextField.Input
+              placeholder="닉네임을 입력해 주세요."
+              {...register("nickname", {
+                onChange: () => setIsValidDuplicateNickname(false),
+              })}
+            />
+            <TextField.Button
+              type="button"
+              disabled={
+                !touchedFields.nickname ||
+                errors.nickname?.type === "invalid_format"
+              }
+              onClick={() => {
+                const nickname = getValues("nickname");
+                checkNickname(nickname)
+                  .then((response) => {
+                    if (response.available) {
+                      setIsValidDuplicateNickname(true);
+                    } else {
+                      setError("nickname", {
+                        type: "duplicate",
+                        message: response.message,
+                      });
+                    }
+                  })
+                  .catch((error) => {
+                    if (error instanceof ApiError) {
+                      if (error.status === HttpStatus.BAD_REQUEST) {
+                        setError("nickname", {
+                          type: "invalid_format",
+                          message: error.message,
+                        });
+                      }
+                    }
                   });
-                }
-              })
-              .catch((error) => {
-                if (error instanceof ApiError) {
-                  if (error.status === HttpStatus.BAD_REQUEST) {
-                    setError("nickname", {
-                      type: "invalid_format",
-                      message: error.message,
-                    });
-                  }
-                }
-              });
-          }}
-        >
-          중복 확인
-        </TextFieldButton>
-      }
-      helperText={
-        <>
-          {!!touchedFields.nickname ? (
-            !!errors.nickname?.message ? (
-              <HelperText state={"error"}>
-                {errors.nickname?.message}
-              </HelperText>
-            ) : (
-              isValidDuplicateNickname && (
-                <HelperText state={"success"}>
-                  사용 가능한 닉네임입니다.
-                </HelperText>
-              )
-            )
-          ) : (
-            <></>
-          )}
-        </>
-      }
-      isError={!!errors.nickname?.message}
-    />
+              }}
+            >
+              중복 확인
+            </TextField.Button>
+          </div>
+          <TextField.HelperText />
+        </div>
+      </TextField>
+    </>
   );
 };
