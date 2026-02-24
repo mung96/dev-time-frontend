bc32a9483fe096ea365f39e00a016d1d4054958b
mung96 <mung5079@gmail.com>
2026-02-24 12:54:47 +0900

    fix: ApiError class 수정

--- src/shared/api/api-requester.ts
+++ src/shared/api/api-requester.ts
@@ -14,9 +14,11 @@ export const apiRequester = async <T = unknown>(
 
   if (!response.ok) {
     //TODO: 서비스 에러말고, 기타 에러 처리해야함
-    const errorData = await response.json();
+    const errorBody = await response.json();
 
-    throw new ApiError(errorData);
+    console.log("errorBody", errorBody);
+
+    throw new ApiError(errorBody);
   }
 
   return response.json() as T;
@@ -33,11 +35,10 @@ interface ErrorData {
 //TODO: 로그인 구현 이후, 다른 API Response보고 수정 필요
 export class ApiError extends Error {
   status: number;
-  message: string;
 
   constructor(errorData: ErrorData) {
-    super(`API Error`);
+    super(errorData.error.message);
+    this.name = "ApiError";
     this.status = errorData.error.statusCode;
-    this.message = errorData.error.message;
   }
 }
