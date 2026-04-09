8a744f5e2021980c8e73b1bb1f54f19fd88ab0aa
SongHyeonMyeong <mung5079@huuim.com>
2026-04-09 10:01:59 +0900

    chore: msw 설치 및 세팅

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,17 @@
+import { server } from "@app/mocks/server";
 import "@testing-library/jest-dom/vitest";
+
+import { vi } from "vitest";
+import { afterAll, afterEach, beforeAll } from "vitest";
+
+const useRouter = vi.fn(() => {
+  push: vi.fn();
+});
+
+vi.mock("next/navigation", () => ({
+  useRouter,
+}));
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
