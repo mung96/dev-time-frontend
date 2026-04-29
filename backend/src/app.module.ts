85b2f7347a1394e94b4a91c82341a52d29e18332
송현명 <mung5079@gmail.com>
2026-04-30 08:24:33 +0900

    backend repo 통합 (#36)
    * init
    
    * Swagger 설정 + Http Exception Filter 구현 + 공통 API 응답 (#1)
    
    * chore: app파일 삭제
    
    * chore: db 설정
    
    * chore: swagger 설정
    
    * feat: exception 필터 구현
    
    * feat: 공통 API Response 클래스 구현
    
    * chore: frontend 폴더로 이동

---
+++ backend/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USERNAME', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', 'postgres'),
+        database: configService.get<string>('DB_DATABASE', 'devtime'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        logging: true,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
