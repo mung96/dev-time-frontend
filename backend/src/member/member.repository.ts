49e9e277aebc65fbc7f8aa8e3030b9836f016fb4
mung96 <mung5079@gmail.com>
2026-05-03 10:39:38 +0900

    feat: email, nickname db레벨 동시성 이슈 예외 처리

--- backend/src/member/member.repository.ts
+++ backend/src/member/member.repository.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { DataAccessException } from 'src/common/exception/data-access.exception';
+import { DuplicateEmailException } from 'src/common/exception/duplicate-email.exception';
+import { DuplicateNicknameException } from 'src/common/exception/duplicate-nickname.exception';
 import { Member } from 'src/member/member.entity';
+import { QueryFailedError, Repository } from 'typeorm';
 
 @Injectable()
 export class MemberRepository {
@@ -22,6 +26,23 @@ export class MemberRepository {
   }
 
   async save(member: Member): Promise<void> {
-    await this.repository.save(member);
+    try {
+      await this.repository.save(member);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        // TODO: 타입 단언 말고 방법이 없나?
+        const { code, detail } = error.driverError as {
+          code: string;
+          detail: string;
+        };
+        if (code === '23505') {
+          if (detail?.includes('email'))
+            throw new DuplicateEmailException(member.email);
+          if (detail?.includes('nickname'))
+            throw new DuplicateNicknameException(member.nickname);
+        }
+      }
+      throw new DataAccessException(error);
+    }
   }
 }
