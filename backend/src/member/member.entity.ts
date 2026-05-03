f90a88dd738cdc740d3037fdb16981d95f0ef787
mung96 <mung5079@gmail.com>
2026-05-03 10:43:11 +0900

    fix: unique email 추가

--- backend/src/member/member.entity.ts
+++ backend/src/member/member.entity.ts
@@ -3,12 +3,12 @@ import { Column, Entity } from 'typeorm';
 
 @Entity()
 export class Member extends BaseModel {
-  @Column({ nullable: false })
+  @Column({ nullable: false, unique: true })
   email: string;
 
   @Column({ nullable: false, unique: true })
   nickname: string;
 
-  @Column({ nullable: false, unique: true, select: false })
+  @Column({ nullable: false, select: false })
   password: string;
 }
