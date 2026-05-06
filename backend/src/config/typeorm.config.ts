020ac93169e012ede8038521620b42e92252943c
mung96 <mung5079@gmail.com>
2026-05-07 08:06:27 +0900

    fix: TypeORM 초기화 에러 수정

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -8,10 +8,10 @@ export const dataSourceOptions = {
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_DATABASE || 'devtime',
-  migrations: ['src/migrations/*.ts'],
 };
 
 export default new DataSource({
   ...dataSourceOptions,
   entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
 });
